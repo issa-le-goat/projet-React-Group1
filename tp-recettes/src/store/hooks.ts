a45e9b507455568c64013e9e4872a2dd98cad33d
issa-le-goat <issa.vebobe@ynov.com>
2026-09-23 15:27:31 +0200

    Delete tp-recettes directory

--- tp-recettes/src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux'
-import type { RootState, AppDispatch } from './store'
-
-// Hooks typés à utiliser partout dans l'app plutôt que useDispatch/useSelector bruts
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
